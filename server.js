f1604a515adef47a705e178ebbe544c57033c66f
unknown <rcohen0511@gmail.com>
2017-12-26 15:57:59 -0500

    change

--- server.js
+++ server.js
@@ -20,6 +20,21 @@ app.get('/', function (req, res) {
     res.send('rcohen0511@gmail.com')
   } else if (question == 'Please provide a phone number we can use to reach you.'){
     res.send('917-416-0409')
+  } else if (question == 'Which position are you applying for?') {
+    res.send('Software Engineer with Fullstack, Backend, or Frontend experience - bRealTime - New York or Chicago')
+  } else if (question == 'How did you hear about this position?'){
+    res.send('AngelList')
+  } else if (question == 'Please list your relevant university degree(s).') {
+    res.send('Pace University, Seidenberg School of Computer Science and Information
+    \nSUNY Binghamton, Harpur College of Arts and Science ')
+  } else if (question == 'Please provide a URL where we can download your resume and cover letter.'){
+    res.send('https://drive.google.com/file/d/1UQB_sFeRWeQ6upbExBmSMIcmYEqntT8T/view?usp=sharing')
+  } else if (question == 'Please provide a URL where we can download the source code of your resume submission web service.') {
+    res.send('https://github.com/rcohen0511/EngineMedia')
+  } else if (question == 'Can you provide proof of eligibility to work in the US?'){
+    res.send('Yes')
+  } else if (question == 'Please solve this puzzle: ABCD A=--- B>--- C->-- D<---') {
+    res.send('')
   }
   // console.log(res);
   res.send('OK')
